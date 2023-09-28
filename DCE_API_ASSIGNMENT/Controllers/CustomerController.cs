a1d6480e5bd91b753dc80dd0f53f817f838ab28d
sniro <snirosh12@gmail.com>
2023-09-28 15:42:11 +0530

    Add project files.

---
+++ DCE_API_ASSIGNMENT/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using DCE_API_ASSIGNMENT.DatabaseAccess;
+using DCE_API_ASSIGNMENT.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DCE_API_ASSIGNMENT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public CustomerController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            List<Customer> customerList = new List<Customer>();
+            customerList = _db.Customer.ToList();
+            return Ok(customerList);
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public IActionResult Create([FromBody] Customer customer)
+        {
+            _db.Add(customer);
+            _db.SaveChanges();
+            return Ok(customer);
+        }
+
+
+
+    }
+}
---
+++ DCE_API_ASSIGNMENT/DatabaseAccess/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DCE_API_ASSIGNMENT.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DCE_API_ASSIGNMENT.DatabaseAccess
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+            
+        }
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
