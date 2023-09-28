1072cba9e29c3ab4f3426f5d9ffb9e9c5dfacd30
sniro <snirosh12@gmail.com>
2023-09-28 16:04:23 +0530

    Calling Methods Created for Create Customer ,Get All Customers , Update Customer ,DELETE Customer .

--- DCE_API_ASSIGNMENT/Controllers/CustomerController.cs
+++ DCE_API_ASSIGNMENT/Controllers/CustomerController.cs
@@ -15,24 +15,108 @@ namespace DCE_API_ASSIGNMENT.Controllers
             _db = db;
         }
 
-        [HttpGet]
-        [Route("GetAll")]
-        public IActionResult GetAll()
-        {
-            List<Customer> customerList = new List<Customer>();
-            customerList = _db.Customer.ToList();
-            return Ok(customerList);
-        }
-
         [HttpPost]
         [Route("Create")]
-        public IActionResult Create([FromBody] Customer customer)
+        public IActionResult CreateCustomer([FromBody] Customer newCustomer)
         {
-            _db.Add(customer);
-            _db.SaveChanges();
-            return Ok(customer);
+            try
+            {
+                if (newCustomer == null)
+                {
+                    return BadRequest("Invalid customer data.");
+                }
+
+                _db.Customer.Add(newCustomer);
+                _db.SaveChanges();
+
+                return Ok(newCustomer);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
         }
 
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAllCustomers()
+        {
+            try
+            {
+                // Retrieve all customers from the database
+                List<Customer> customerList = _db.Customer.ToList();
+
+                return Ok(customerList);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
+
+        [HttpPost] 
+        [Route("Update")]
+        public IActionResult UpdateCustomer([FromBody] Customer updatedCustomer)
+        {
+            try
+            {
+                // Check if the provided customer data is valid
+                if (updatedCustomer == null)
+                {
+                    return BadRequest("Invalid customer data.");
+                }
+
+                // Retrieve the existing customer from the database based on UserId
+                Customer existingCustomer = _db.Customer.FirstOrDefault(c => c.UserId == updatedCustomer.UserId);
+
+                // Check if the customer exists
+                if (existingCustomer == null)
+                {
+                    return NotFound("Customer not found.");
+                }
+
+                // Update the customer's properties with the new values
+                existingCustomer.Username = updatedCustomer.Username;
+                existingCustomer.Email = updatedCustomer.Email;
+                existingCustomer.FirstName = updatedCustomer.FirstName;
+                existingCustomer.LastName = updatedCustomer.LastName;
+                existingCustomer.IsActive = updatedCustomer.IsActive;
+
+                _db.SaveChanges();
+
+                return Ok("Customer updated successfully.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public IActionResult DeleteCustomer(Guid id)
+        {
+            try
+            {
+                // Find the customer by ID
+                Customer customerToDelete = _db.Customer.FirstOrDefault(c => c.UserId == id);
+
+                if (customerToDelete == null)
+                {
+                    return NotFound("Customer not found.");
+                }
+
+                _db.Customer.Remove(customerToDelete);
+                _db.SaveChanges();
+
+                return Ok("Customer deleted successfully.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
 
 
     }
