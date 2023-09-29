1067f58e73d5f636910ef6647215e956f66ba669
sniro <snirosh12@gmail.com>
2023-09-29 13:09:17 +0530

    All Active order details endpoint created

---
+++ DCE_API_ASSIGNMENT/Models/ActiverOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DCE_API_ASSIGNMENT.Models
+{
+    public class ActiverOrder
+    {
+        [Key]
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public Guid OrderBy { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public Guid SupplierId { get; set; }
+        public string SupplierName { get; set; }
+    }
+}
